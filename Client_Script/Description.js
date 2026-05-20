9f15893bf003192d51478a33e0ef54bc08772fbf
NoodleBoodle <noodleboodle2501@gmail.com>
2026-05-19 20:26:03 -0400

    Create Description.js

---
+++ Client_Script/Description.js
@@ -0,0 +1,39 @@
+ItemEvents.modifyTooltips(event => {
+
+// Steel Tools 
+  event.modify('tfmg:steel_sword', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('tfmg:steel_pickaxe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('tfmg:steel_axe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('tfmg:steel_shovel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('tfmg:steel_hoe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('immersiveengineering:pickaxe_steel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('immersiveengineering:shovel_steel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('immersiveengineering:axe_steel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('immersiveengineering:hoe_steel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('immersiveengineering:sword_steel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+
+
+
+
+
+})
