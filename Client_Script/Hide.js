348612e32df3824a74f36c2a7c0cc1cf25de934d
NoodleBoodle <noodleboodle2501@gmail.com>
2026-05-19 20:25:14 -0400

    Create Hide.js

---
+++ Client_Script/Hide.js
@@ -0,0 +1,15 @@
+KubeJEIEvents.subtypes(event => {
+
+
+//steel
+ event.hide(Item.of('createbigcannons:steel_ingot'))
+ event.hide(Item.of('alloyed:steel_ingot'))
+ event.hide(Item.of('alloyed:steel_nugget'))
+ event.hide(Item.of('alloyed:steel_sheet'))
+ event.hide(Item.of('alloyed:steel_block'))
+ event.hide(Item.of('createbigcannons:steel_block'))
+ event.hide(Item.of('immersiveengineering:wirecoil_structure_steel'))
+ event.hide(Item.of('immersiveengineering:creosote'))
+
+
+})
