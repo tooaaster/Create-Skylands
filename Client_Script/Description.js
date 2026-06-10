ff9a0b9a0cbdfca9d22d170d8eb85c60c29f01c9
NoodleBoodle <noodleboodle2501@gmail.com>
2026-06-09 20:56:40 -0400

    Update Description.js

--- Client_Script/Description.js
+++ Client_Script/Description.js
@@ -31,9 +31,68 @@ ItemEvents.modifyTooltips(event => {
   event.modify('immersiveengineering:sword_steel', text => {
     text.add(Text.red("Uncraftable"))
 })
-
-
-
-
-
+  event.modify('overgeared:steel_helmet', text => {
+    text.add(Text.red("Uncraftable"))
 })
+  event.modify('overgeared:steel_chestplate', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_leggings', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_boots', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_sword', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_pickaxe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_axe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_shovel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('overgeared:steel_hoe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_helmet', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_chestplate', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_leggings', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_boots', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_pickaxe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_axe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_sword', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_shovel', text => {
+    text.add(Text.red("Uncraftable"))
+})
+  event.modify('create_sa:copper_hoe', text => {
+    text.add(Text.red("Uncraftable"))
+})
+
+
+
+
+
+
+
+
+
+
+})// end of event
