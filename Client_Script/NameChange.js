8c184217d87c56953e2c4b1b6dd9082cce7f7253
tooaaster <skytooaaster@gmail.com>
2026-06-22 17:05:10 -0500

    Add files via upload

--- Client_Script/NameChange.js
+++ Client_Script/NameChange.js
@@ -1,10 +1,16 @@
 ClientEvents.lang("en_us", event => {
-    event.renameBlock('createendergateway:abandoned_gateway', 'Gateway Core')
     //event.renameItem('minecraft:', '')
+    event.renameBlock('createendergateway:abandoned_gateway', 'Gateway Core')
     event.renameItem('immersiveengineering:armor_steel_helmet', 'Heavy Steel Helmet')
     event.renameItem('immersiveengineering:armor_steel_chestplate', 'Heavy Steel Chestplate')
     event.renameItem('immersiveengineering:armor_steel_leggings', 'Heavy Steel Legging')
     event.renameItem('immersiveengineering:armor_steel_boots', 'Heavy Steel Boots')
     event.renameItem('tfmg:rubber_sheet', 'Petroleum Rubber Sheet')
     event.renameItem('tfmg:silicon_ingot', 'Pure Silicon Ingot')
+    event.renameItem('createpropulsion:platinum_casing', 'Certus Quartz Casing')
+    event.renameItem('create_connected:empty_fan_catalyst', 'Brass Frame')
+    event.renameItem('ae2:silicon', 'Certus Silicon')
+    event.renameItem('ae2:printed_silicon', 'Certus Silicon Plate')
 })
+
+
